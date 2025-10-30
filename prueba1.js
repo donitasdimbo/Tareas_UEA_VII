cde269084979352d4eedfdac779d9a31f78e68e5
donitasdimbo <2233034021@correo.ler.uam.mx>
2025-10-30 12:40:27 -0600

    mmm

--- prueba1.js
+++
@@ -1,5 +0,0 @@
-let texto = '{"cuerpo" : ["ojos", "boca", "manos", "orejas"],  "piezas" : ["camara", "sensores", "metal", "microfonos"]}';
-
-const info = JSON.parse (texto) ;
-
-document.getElementById("hola").innerHTML = "cuerpo = " + info.cuerpo[1] + " " + info.piezas[1];
