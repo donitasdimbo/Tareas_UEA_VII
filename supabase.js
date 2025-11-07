97a0a99f94a0c4db4dc891d14dbb705ba49a728c
donitasdimbo <2233034021@correo.ler.uam.mx>
2025-11-07 09:48:45 -0600

    mas trabajo yei

--- supabase.js
+++ supabase.js
@@ -1,4 +1,11 @@
-import { createClient } from '@supabase/supabase-js'
+//import { createClient } from '@supabase/supabase-js'
 
 // Create a single supabase client for interacting with your database
-const supabase = createClient('https://xyzcompany.supabase.co', 'publishable-or-anon-key')
+const supabaseClient = supabase.createClient('https://gqqikxsrfkwcmnvpcwbv.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdxcWlreHNyZmt3Y21udnBjd2J2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE4OTk3OTcsImV4cCI6MjA3NzQ3NTc5N30.2y9dxp883o4Nkz6twH-u7sqYCpzdfam12olg10-ar4E', {
+    db:{
+        schema: 'public'
+    }
+});
+
+
+console.log(supabaseClient)
